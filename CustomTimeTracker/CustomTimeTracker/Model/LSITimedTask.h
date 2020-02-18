afc6e42d5731a2ca9712669f3e107c8eea936bc6
Troy Christian <apostasy8@gmail.com>
2020-02-18 17:26:16 -0500

    LSITimedTask defined in Header File

---
+++ CustomTimeTracker/CustomTimeTracker/Model/LSITimedTask.h
@@ -0,0 +1,17 @@
+//
+//  LSITimedTask.h
+//  CustomTimeTracker
+//
+//  Created by Lambda_School_Loaner_219 on 2/18/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomTimeTracker/CustomTimeTracker/Model/LSITimedTask.m
@@ -0,0 +1,13 @@
+//
+//  LSITimedTask.m
+//  CustomTimeTracker
+//
+//  Created by Lambda_School_Loaner_219 on 2/18/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import "LSITimedTask.h"
+
+@implementation LSITimedTask
+
+@end
