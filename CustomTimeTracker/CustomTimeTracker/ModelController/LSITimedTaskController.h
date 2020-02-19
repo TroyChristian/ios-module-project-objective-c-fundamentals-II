9bfe3f9a6efaf06e87c4154a176d17bcde80a01b
Troy Christian <apostasy8@gmail.com>
2020-02-19 01:13:53 -0500

    Add TimedTaskController

---
+++ CustomTimeTracker/CustomTimeTracker/ModelController/LSITimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  LSITimedTaskController.h
+//  CustomTimeTracker
+//
+//  Created by Lambda_School_Loaner_219 on 2/19/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomTimeTracker/CustomTimeTracker/ModelController/LSITimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  LSITimedTaskController.m
+//  CustomTimeTracker
+//
+//  Created by Lambda_School_Loaner_219 on 2/19/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import "LSITimedTaskController.h"
+
+@implementation LSITimedTaskController
+
+@end
